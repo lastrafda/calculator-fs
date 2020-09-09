bbef4c1a0b3b677af5908e3ec436661390c080c3
José Lastra <lastrafda@gmail.com>
2020-09-09 03:54:59 -0300

    extract the reducer into its own file
    This way I'll be able to make unit tests over the state
    and just import the reducer without caring about
    mounting components.
    
    I heard of this from Steve Kinney on Frontend Masters.

--- src/components/Calculator.js
+++ src/components/Calculator.js
@@ -3,7 +3,7 @@ import "./Calculator.css";
 import PropTypes from "prop-types";
 import Button from "./Button";
 import Display from "./Display";
-import { getCalculation } from "./../util/util";
+import reducer from "./Calculator.reducer";
 
 function init(value) {
   return {
@@ -11,52 +11,6 @@ function init(value) {
     operator: "",
     previous: "",
   };
-}
-
-function reducer(state, action) {
-  const { payload, type } = action;
-  const currentHasNoDots = state.current.indexOf(".") === -1;
-  let result;
-
-  switch (type) {
-    case "allClear":
-      return {
-        current: "",
-        operator: "",
-        previous: "",
-      };
-    case "append":
-      if (payload.value === "." && currentHasNoDots) {
-        const formatedDot = state.current.length ? "." : "0.";
-        return { ...state, current: state.current + formatedDot };
-      }
-      if (payload.value === "0" && +state.current === 0 && currentHasNoDots) {
-        return { ...state };
-      }
-      if (payload.value !== "0" && +state.current === 0 && currentHasNoDots) {
-        return { ...state, current: payload.value };
-      }
-      if (payload.value !== ".") {
-        return { ...state, current: state.current + payload.value };
-      }
-      return { ...state };
-    case "calculate":
-      result = getCalculation(state.previous, state.current, state.operator);
-      return { current: result, operator: "", previous: "" };
-    case "setOperator":
-      // if there isn't an operation in course then change the operator
-      if (state.previous === "") {
-        return {
-          current: state.previous,
-          operator: payload.operator,
-          previous: state.current,
-        };
-      }
-      return { ...state };
-    default:
-      console.log(`Action Type: "${action.type} is not currently implemented"`);
-      return { ...state };
-  }
 }
 
 const Calculator = ({ initialValue }) => {
